a11facc71a8a188e0b83118dcb87aecf8185e525
ressbill <51719448+ressbill@users.noreply.github.com>
2020-06-27 14:37:18 +0200

    forms created (#3)

--- front/src/components/Form/Form.js
+++ front/src/components/Form/Form.js
@@ -1,45 +1,101 @@
-import React, { useState } from "react"
+import React, { useState, createRef, useEffect } from "react"
 import Button from "react-bootstrap/Button"
-import  Form from 'react-bootstrap/Form'
-import classes from './Form.module.scss'
 
-export const AuthForm = ({register}) => {
-    const [validated, setValidated] = useState(false)
-    const submitHandler = (event) => {
-      event.preventDefault()
-      const form = event.target
-      if(form.checkValidity()) {
-        // make request to server
-      }
+export const AuthForm = ({ register }) => {
+  const [formInputs, setFormInputs] = useState({ email: "", password: "" })
+  const [validEmail, setValidEmail] = useState(false)
+  const [validPassword, setValidPassword] = useState(false)
+  const [formValidated, setFormValidated] = useState(false)
 
+  const submitHandler = (event) => {
+    setFormValidated(true)
+    setValidEmail(isEmail(formInputs.email))
+    setValidPassword(formInputs.password.length >=5)
+    event.preventDefault()
+    if (validPassword && validEmail) {
+      // request to server depends on register props
     }
-    return (
-      <Form
-        style={{ marginTop: "10rem" }}
-        validated={validated}
-        onSubmit={submitHandler}
-      >
-        <Form.Group controlId='formBasicEmail' className={classes.Controls}>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type='email' required placeholder='Enter email' />
-          <Form.Control.Feedback type='invalid'>
-            Please choose a username.
-          </Form.Control.Feedback>
-        </Form.Group>
+  }
 
-        <Form.Group controlId='formBasicPassword' className={classes.Controls}>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            required
-            minLength='5'
-            placeholder='Password'
-          />
-        </Form.Group>
-        <Button variant='primary' type='submit' style={{ float: "right" }}>
-          {register ? "Зарегистрироваться " : "Войти"}
-        </Button>
-      </Form>
-    )
+  const onEmailChange = (event) => {
+    const emailValue = event.target.value
+    setValidEmail(isEmail(event.target.value))
+    setFormInputs((prev) => {
+      return { ...prev, email: emailValue }
+    })
+  }
+  const onPasswordChange = (event) => {
+    const passwordValue = event.target.value
+    setValidPassword(passwordValue.length >= 5)
+    setFormInputs((prev) => {
+      return { ...prev, password: passwordValue }
+    })
+  }
+
+  return (
+    <form style={{ marginTop: "10rem" }} onSubmit={submitHandler}>
+      <label htmlFor='email'>Enter email:</label>
+      <div className='input-group mb-4'>
+        <div className='input-group-prepend'>
+          <span className='input-group-text' id='basic-addon1'>
+            @
+          </span>
+        </div>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='u@mail.com'
+          aria-label='Email'
+          id='email'
+          onChange={onEmailChange}
+        />
+      </div>
+      {!validEmail && formValidated ? (
+        <span
+          style={{
+            display: "block",
+            fontSize: "0.9rem",
+            color: "red",
+            marginTop: "-1rem",
+          }}
+        >
+          Please enter a valid email
+        </span>
+      ) : null}
+
+      <label htmlFor='password'>Password:</label>
+      <div className='input-group mb-3'>
+        <div className='input-group-prepend'></div>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Password'
+          aria-label='Password'
+          id='password'
+          onChange={onPasswordChange}
+        />
+      </div>
+      {!validPassword && formValidated ? (
+        <span
+          style={{
+            display: "block",
+            fontSize: "0.9rem",
+            color: "red",
+            marginTop: "-0.5rem",
+          }}
+        >
+          Password must contain at least 5 charachters
+        </span>
+      ) : null}
+
+      <Button variant='primary' type='submit' style={{ float: "right" }}>
+        {register ? "Sign in" : "Login"}
+      </Button>
+    </form>
+  )
 }
 
+function isEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
+}
